c88edb0a4ba6f2c2b0620ed05c917b62391b07b3
LUCASDALLACOSTA <lucas.dallacosta@limayrac.fr>
2023-07-11 17:29:59 +0200

    Initial commit

---
+++ gestion_materiel/models.py
@@ -0,0 +1,102 @@
+from django.db import models
+
+class Enseignant(models.Model):
+    nom = models.CharField(
+        max_length=50,
+        blank=False,
+        null=True,
+        default='aucun'
+    )
+    prenom = models.CharField(
+        max_length=50,
+        blank=False,
+        null=True,
+        default='aucun'
+    )
+
+class Salle(models.Model):
+    nom = models.CharField(
+        max_length=50,
+        blank=False,
+        null=True,
+        default='aucun'
+    )
+    etage = models.CharField(
+        max_length=50,
+        blank=False,
+        null=True,
+        default='aucun'
+    )
+
+
+class Materiel(models.Model):
+    nom = models.CharField(
+        max_length=100,
+        blank=False,
+        null=True,
+        default='aucun'
+    )
+    budget = models.CharField(
+        max_length=100,
+        blank=False,
+        null=True,
+        default='aucun'
+    )
+    proprietaire = models.ForeignKey(
+        Enseignant,
+        on_delete=models.CASCADE,
+        null=True,
+        related_name='materiels_possedes'  # Accesseur inverse unique pour la relation ForeignKey proprietaire
+    )
+    lieu = models.ForeignKey(
+        Salle,
+        on_delete=models.CASCADE,
+        null=True
+    )
+    possesseur = models.ForeignKey(
+        Enseignant,
+        on_delete=models.CASCADE,
+        null=True,
+        related_name='materiels_en_possession'  # Accesseur inverse unique pour la relation ForeignKey possesseur
+    )
+
+class TransfertMateriel(models.Model):
+    material = models.ForeignKey(
+        Materiel,
+        on_delete=models.CASCADE,
+        null=True
+    )
+    ancien_possesseur = models.ForeignKey(
+        Enseignant,
+        on_delete=models.CASCADE,
+        null=True,
+        related_name='transferts_sortants'  # Accesseur inverse unique pour la relation ForeignKey ancien_possesseur
+    )
+    nouveau_possesseur = models.ForeignKey(
+        Enseignant,
+        on_delete=models.CASCADE,
+        null=True,
+        related_name='transferts_entrants'  # Accesseur inverse unique pour la relation ForeignKey nouveau_possesseur
+    )
+    date_transfert = models.DateField()
+    lieu_transfer = models.CharField(
+        max_length=100
+    )
+    occasion = models.CharField(
+        max_length=100
+    )
+    objectif = models.TextField()
+
+class AccessoireMateriel(models.Model):
+    material = models.ForeignKey(
+        Materiel,
+        on_delete=models.CASCADE,
+        null=True
+    )
+    nom = models.CharField(
+        max_length=100
+    )
+    present = models.BooleanField(
+        default=True
+    )
+    etat = models.CharField(max_length=100)
